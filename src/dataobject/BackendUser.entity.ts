0bf6af0c02eef07657102825e47d2936efc3cf95
“always” <740905172@qq.com>
2020-06-18 11:50:18 +0800

    修改用户entity类型

--- src/dataobject/BackendUser.entity.ts
+++ src/dataobject/BackendUser.entity.ts
@@ -2,25 +2,45 @@
  * @Author: Always
  * @LastEditors: Always
  * @Date: 2020-06-01 18:53:23
- * @LastEditTime: 2020-06-15 17:45:43
+ * @LastEditTime: 2020-06-18 11:46:55
  * @FilePath: /koala-background-server/src/dataobject/BackendUser.entity.ts
  */
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
-import { EBackendUserType } from '../enums/EBackendUserType';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import {
+  EBackendUserType,
+  EbackendFindWithUserType,
+} from '../enums/EBackendUserType';
 
 @Entity('tb_backend_user')
 export class BackendUser {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn({ comment: '用户id' })
   userId: number;
 
-  @Column({ length: 255 })
+  @Column({ length: 255, comment: '用户名' })
   username: string;
 
-  @Column({ length: 255 })
+  @Column({ length: 255, comment: '用户密码' })
   password: string;
 
-  @Column()
+  @Column({
+    type: 'enum',
+    enum: EBackendUserType,
+    default: EbackendFindWithUserType.PROXY,
+    comment: '用户类型',
+  })
   userType: EBackendUserType;
+
+  @CreateDateColumn({ comment: '创建时间' })
+  createTime: string;
+
+  @UpdateDateColumn({ comment: '更新时间' })
+  updateTime: string;
 
   constructor(
     userId: number,
