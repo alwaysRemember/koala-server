0962339a9bfadc2d7bb8c4a2bea1f078230026bb
“always” <740905172@qq.com>
2020-12-07 18:32:14 +0800

    add => prod 环境

---
+++ src/config/projectConfig.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: Always
+ * @LastEditors: Always
+ * @Date: 2020-06-23 16:01:37
+ * @LastEditTime: 2020-10-27 18:32:34
+ * @FilePath: /koala-server/src/config/projectConfig.ts
+ */
+
+export const appId = 'wx0fd579235e1edc42';
+export const AppSecret = '5c7dbeb1d52420d6c939965066ceac93';
+export const mchId = '1601117723';
+export const mchApiKey = "xoXNQaLVMD1DusMmpHSi5110r7EyV8I2";
+export const KUAIDI_100_KEY = "YwZHdrsb6274";
+export const KUADI_100_USER_ID = "517082ba163b457787a7ab5ac0528f17";
