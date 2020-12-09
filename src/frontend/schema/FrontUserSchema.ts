efdb34f48540b325dc23d068d5d60f570e07c5a2
“always” <740905172@qq.com>
2020-12-09 11:55:41 +0800

    fix => 修复当小程序用户授权登录的时候不存在某些信息的时候不校验

--- src/frontend/schema/FrontUserSchema.ts
+++ src/frontend/schema/FrontUserSchema.ts
@@ -2,7 +2,7 @@
  * @Author: Always
  * @LastEditors: Always
  * @Date: 2020-06-23 15:21:13
- * @LastEditTime: 2020-11-13 15:37:17
+ * @LastEditTime: 2020-12-09 11:55:00
  * @FilePath: /koala-server/src/frontend/schema/FrontUserSchema.ts
  */
 
@@ -10,14 +10,14 @@ import * as Joi from '@hapi/joi';
 import { EUserGender, EUserLanguage } from '../../global/enums/EUserGlobal';
 export const UserLoginSchema = Joi.object({
   code: Joi.string().required(),
-  nickName: Joi.string().required(),
-  avatarUrl: Joi.string().required(),
+  nickName: Joi.string(),
+  avatarUrl: Joi.string(),
   gender: Joi.number()
     .valid(EUserGender.UNKONWN, EUserGender.MAN, EUserGender.WOMAN)
     .default(EUserGender.UNKONWN),
-  country: Joi.string().required(),
-  province: Joi.string().required(),
-  city: Joi.string().required(),
+  country: Joi.string(),
+  province: Joi.string(),
+  city: Joi.string(),
   language: Joi.string()
     .valid(EUserLanguage.EN, EUserLanguage.ZH_CN, EUserLanguage.ZH_TW)
     .default(EUserLanguage.EN),
